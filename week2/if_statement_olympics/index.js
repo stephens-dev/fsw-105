611a6b3e0a94fd9f1e8406dddf25b6558406a5a0
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-08-05 20:38:37 -0500

    week2

---
+++ week2/if_statement_olympics/index.js
@@ -0,0 +1,25 @@
+if (5 > 3){
+    console.log("is greater than")
+}else{
+    console.log("not greater than")
+}
+
+var word = "cat"
+var n = word.length
+if (n === 3){
+    console.log("is the length")
+}else{
+    console.log("is not the length")
+}
+
+if ("cat" === "dog"){
+    console.log("is the same")
+}else{
+    console.log("not the same")
+}
+
+var age;
+
+function checkelegibiliy(){
+    
+}
