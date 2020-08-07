16a4c1eebbec1faeb42f714b40b843d722dc51ae
stephensWebDev <53384838+stephensWebDev@users.noreply.github.com>
2020-08-06 20:25:22 -0500

    week2

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -1,25 +1,77 @@
-if (5 > 3){
+// Preliminaries:
+if (5 > 3) {
     console.log("is greater than")
-}else{
+} else {
     console.log("not greater than")
 }
 
 var word = "cat"
 var n = word.length
-if (n === 3){
+if (n === 3) {
     console.log("is the length")
-}else{
+} else {
     console.log("is not the length")
 }
 
-if ("cat" === "dog"){
+if ("cat" === "dog") {
     console.log("is the same")
-}else{
+} else {
     console.log("not the same")
 }
+// Bronze Medal
+var person = {
+    name: "Bobby",
+    age: 12
+}
+if (person.age >= 18) {
+    console.log("can see the movie")
+} else {
+    console.log("cannot see the movie")
+}
 
-var age;
+if (person.name.charAt(0) === "B") {
+    console.log("can see the movie")
+} else {
+    console.log("cannot see the movie")
+}
 
-function checkelegibiliy(){
-    
+if (person.age >= 18 && person.name.charAt(0) === "B") {
+    console.log("Can see the movie")
+} else {
+    console.log("Cannot see the movie")
+} 
+// Silver Medal
+if (1 === "1") {
+    console.log("strict")
+}else if (1 == "1"){
+    console.log("loose")
+}else {
+    console.log("dose not equal at all")
+}
+
+if (1 <= 2 && 2 == 4) {
+    console.log("yes")
+}else{
+    console.log("no")
+}
+// Gold Medal
+var dog = "dog"
+console.log(typeof dog)
+
+if ("true" === Boolean) {
+console.log("Boolean")
+}else{
+    console.log("not a Boolean")
+}
+
+if ("s" > 12){
+    console.log("is greater than")
+}else {
+    console.log("is not greater than")
+}
+
+if ("ssdfg" > 12454){
+    console.log("is greater than")
+}else {
+    console.log("is not greater than")
 }
